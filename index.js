a481116a291d22e89592103a5402992a6f8b078f
HediKhemiri3001 <khemirimedhedi@gmail.com>
2022-04-27 16:13:14 +0100

    Added pub sub

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-import { GraphQLServer } from 'graphql-yoga'
-import { Query } from './resolvers/Query.js'
-import { Student } from './resolvers/Student.js'
-import { User } from './resolvers/User.js'
-import { Todo } from './resolvers/Todo.js'
+import { GraphQLServer } from "graphql-yoga";
+import { Query } from "./resolvers/Query.js";
+import { Student } from "./resolvers/Student.js";
+import { User } from "./resolvers/User.js";
+import { Todo } from "./resolvers/Todo.js";
+import { Subscription } from "./resolvers/Subscription.js";
+import { Mutation } from "./resolvers/Mutation.js";
 // ... or using "require()"
 // const { GraphQLServer } = require('graphql-yoga')
 const typeDefs = "schema/schema.graphql";
 const resolvers = {
-    Query,
-    Student,
-    User,
-    Todo
+  Query,
+  Student,
+  User,
+  Todo,
+  Mutation,
+  Subscription,
 };
-const server = new GraphQLServer({ typeDefs, resolvers })
-server.start(() => console.log('Server is running on localhost:4000'))
+const server = new GraphQLServer({ typeDefs, resolvers });
+server.start(() => console.log("Server is running on localhost:4000"));
